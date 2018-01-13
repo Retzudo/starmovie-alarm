f513fa4721bb3fcfac52ba74278c0eb3055c0b7d
Retzudo <christoph.willinger@gmail.com>
2018-01-14 00:23:31 +0100

    Movies view

--- starmoviealert/frontend/views.py
+++ starmoviealert/frontend/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 
-from core.models import Starmovie
+from core.models import Starmovie, Movie
 
 
 def index(request):
@@ -18,8 +18,9 @@ def index(request):
 def show_movies(request, location):
     starmovie = get_object_or_404(Starmovie, location=location)
 
-    movies = starmovie.movies.filter(
-        showing_dates__date__gte=datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
+    movies = Movie.objects.filter(
+        showing_dates__date__gte=datetime.now(pytz.timezone(settings.TIME_ZONE)),
+        showing_dates__location__location__iexact=location,
     ).distinct()
 
     return render(request, 'frontend/starmovie.html', context={
